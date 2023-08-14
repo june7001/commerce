13f8652f7e7a330b82e6501054daf579b28bc12b
June <june.zhao@yahoo.com>
2023-08-14 21:03:43 +0200

    remove excessive classes and make elements black

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -40,7 +40,7 @@ export default function Search() {
         autoComplete="off"
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
-        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+        className="w-full border border-black bg-transparent px-4 py-2 text-sm text-black placeholder:text-black/50  "
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
