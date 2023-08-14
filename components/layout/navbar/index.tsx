bed6321ecdcd3a8fff6c89935d527fecf6c0caa4
June <june.zhao@yahoo.com>
2023-08-14 20:50:05 +0200

    fix navbar positioning

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -13,7 +13,7 @@ export default async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
 
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
+    <nav className="fixed z-10 flex w-full items-center justify-between p-4 lg:px-6">
       <div className="block flex-none md:hidden">
         <MobileMenu menu={menu} />
       </div>
