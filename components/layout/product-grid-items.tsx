530597ebeffa58608873788dcc7fef1eaaf061b6
June <june.zhao@yahoo.com>
2023-08-16 13:45:35 +0200

    refactor tile component

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -7,7 +7,7 @@ export default function ProductGridItems({ products }: { products: Product[] }) 
   return (
     <>
       {products.map((product) => (
-        <Grid.Item key={product.handle} className="animate-fadeIn">
+        <Grid.Item key={product.handle} className="mb-4 animate-fadeIn">
           <Link className="relative inline-block h-full w-full" href={`/product/${product.handle}`}>
             <GridTileImage
               alt={product.title}
@@ -17,7 +17,8 @@ export default function ProductGridItems({ products }: { products: Product[] }) 
                 currencyCode: product.priceRange.maxVariantPrice.currencyCode
               }}
               src={product.featuredImage?.url}
-              fill
+              height={300}
+              width={300}
               sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
             />
           </Link>
