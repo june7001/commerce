d13648549c0bd09b11aab3b3d5d82b59effcefb2
June <june.zhao@yahoo.com>
2023-08-18 15:20:41 +0200

    remove tax display in cart modal

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -150,14 +150,6 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                     })}
                   </ul>
                   <div className="py-4 text-sm text-neutral-500 ">
-                    <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 ">
-                      <p>Taxes</p>
-                      <Price
-                        className="text-right text-base text-black "
-                        amount={cart.cost.totalTaxAmount.amount}
-                        currencyCode={cart.cost.totalTaxAmount.currencyCode}
-                      />
-                    </div>
                     <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 ">
                       <p>Shipping</p>
                       <p className="text-right">Calculated at checkout</p>
