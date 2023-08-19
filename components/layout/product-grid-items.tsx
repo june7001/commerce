a02c91030efd76ce3a40be11482fb25dc9967537
June <june.zhao@yahoo.com>
2023-08-19 02:20:33 +0200

    display 2 tiles on mobile with adjusted margins

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -7,7 +7,7 @@ export default function ProductGridItems({ products }: { products: Product[] }) 
   return (
     <>
       {products.map((product) => (
-        <Grid.Item key={product.handle} className="mb-4 animate-fadeIn">
+        <Grid.Item key={product.handle} className="mb-10 animate-fadeIn md:mb-6">
           <Link className="relative inline-block h-full w-full" href={`/product/${product.handle}`}>
             <GridTileImage
               alt={product.title}
