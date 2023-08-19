e2723d86fd20534e3c994d6a849d68b6ab4fce92
June <june.zhao@yahoo.com>
2023-08-19 02:07:16 +0200

    resize related product tiles for mobile

--- components/label.tsx
+++ components/label.tsx
@@ -10,7 +10,7 @@ const Label = ({
   currencyCode: string;
 }) => {
   return (
-    <div className="mt-2 flex w-full justify-between">
+    <div className="mt-2 flex w-full flex-col justify-between md:flex-row">
       <h3 className="uppercase text-green-900">{title}</h3>
       <Price amount={amount} currencyCode={currencyCode} />
     </div>
